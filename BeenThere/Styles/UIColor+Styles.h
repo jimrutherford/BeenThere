687e9b9e1d9d101fd081363ebec29fbce0461856
Jim Rutherford <jim@taptonics.com>
2014-10-13 23:00:19 -0700

    Styles

---
+++ BeenThere/Styles/UIColor+Styles.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+Styles.h
+//  BeenThere
+//
+//  Created by Jim Rutherford on 2014-10-13.
+//  Copyright (c) 2014 Taptonics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor(Styles)
+
++(UIColor*) btMediumBlueColor;
++(UIColor*) btDarkBlueColor;
+
++(UIColor*) btDarkRedColor;
++(UIColor*) btDeepRedColor;
+
+@end
